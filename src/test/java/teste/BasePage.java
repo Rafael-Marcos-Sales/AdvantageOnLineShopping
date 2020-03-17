4dbcd727d68438e469e4a469097a3dec6f8f5aa7
Rafael Marcos Sales <rafael.sales@rsinet.com.br>
2020-03-17 11:56:53 -0300

    Criando BasePage

---
+++ src/test/java/teste/BasePage.java
@@ -0,0 +1,37 @@
+package teste;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BasePage {
+	
+	public static WebDriver driver;
+	
+	public BasePage (WebDriver driver) {
+		BasePage.driver = driver;
+		
+	}
+	
+	public void inicio() {
+		System.setProperty("webdriver.chrome.driver" , "C:/chromedriver.exe");
+		
+		driver = new ChromeDriver();
+		driver.get("https://www.advantageonlineshopping.com/#/");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(15,TimeUnit.SECONDS);
+		
+	}
+	
+	public void finaliza() {
+		driver.quit();
+		
+	}
+	
+	public WebDriver getDriver() {
+		return driver;
+	}
+
+}
+
