ea7db474ff8510a3861a5187283cd01fb0ca7636
rafael.sales <rafael.sales@SKAR1201137.rsinet.com.br>
2020-03-27 17:05:08 -0300

    Continuação da "CREATE NEW ACCOUNT"

--- src/test/java/br/com/rsinet/hub/tdd/pageObjects/CriarConta_PO.java
+++ src/test/java/br/com/rsinet/hub/tdd/pageObjects/CriarConta_PO.java
@@ -1,22 +1,26 @@
 package br.com.rsinet.hub.tdd.pageObjects;
 
+import org.junit.Before;
+import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 
 public class CriarConta_PO {
 	
 	private WebDriver driver;
-
+	
+   
 	public CriarConta_PO(WebDriver driver) {
 		this.driver = driver;
 	}
+	
 	
 	public void criarConta() {
 		
 		//clicar em btnusuario
 		WebElement btnusuario = driver.findElement(By.id("menuUser"));
-				
 		btnusuario.click();
 		try {
 			Thread.sleep(5000);
@@ -26,7 +30,6 @@ public class CriarConta_PO {
 		}
 		
 		//Clicar em criar btncriarConta
-		
 		WebElement btncriarConta = driver.findElement(By.xpath("//a[text()='CREATE NEW ACCOUNT']"));
 		btncriarConta.click();
 		
@@ -34,6 +37,42 @@ public class CriarConta_PO {
 		WebElement txtusernameRegisterPage = driver.findElement(By.name("usernameRegisterPage"));
 		WebElement txtemailRegisterPage = driver.findElement(By.name("emailRegisterPage"));
 		WebElement txtpasswordRegisterPage = driver.findElement(By.name("passwordRegisterPage"));
+		WebElement txtconfirm_passwordRegisterPage = driver.findElement(By.name("confirm_passwordRegisterPage"));
+		txtusernameRegisterPage.sendKeys("Rafael.Sales");
+		txtemailRegisterPage.sendKeys("rms@gmail.com");
+		txtpasswordRegisterPage.sendKeys("Aa123@");
+		txtconfirm_passwordRegisterPage.sendKeys("Aa123@");
+		
+		
+		
+		//Preencher detalhes pessoais
+		WebElement txtfirstnameRegisterPage = driver.findElement(By.name("first_nameRegisterPage"));
+		WebElement txtlast_nameRegisterPage = driver.findElement(By.name("last_nameRegisterPage"));
+		WebElement txtphone_numberRegisterPage = driver.findElement(By.name("phone_numberRegisterPage"));
+		txtfirstnameRegisterPage.sendKeys("Rafael Marcos");
+		txtlast_nameRegisterPage.sendKeys("Sales");
+		txtphone_numberRegisterPage.sendKeys("011-982345678");
+		
+		
+		//Preencher Endereço Global
+				
+		WebElement CheckBoxcountryListboxRegisterPage = driver.findElement(By.name("countryListboxRegisterPage"));
+		Select Pais = new Select(CheckBoxcountryListboxRegisterPage);
+		WebElement cityRegisterPage = driver.findElement(By.name("cityRegisterPage"));
+		WebElement addressRegisterPage = driver.findElement(By.name("addressRegisterPage"));
+		WebElement txtestado = driver.findElement(By.name("state_/_province_/_regionRegisterPage"));
+		WebElement postal_codeRegisterPage = driver.findElement(By.name("postal_codeRegisterPage"));
+		
+
+		//WebDriverWait wait = new WebDriverWait(driver, 70);
+		
+				
+		Pais.selectByVisibleText("Brazil");
+		cityRegisterPage.sendKeys("São Paulo");
+		addressRegisterPage.sendKeys("Rua Pio XII,245");
+		txtestado.sendKeys("São Paulo");
+		postal_codeRegisterPage.sendKeys("01322-030");
+		
 	}
 	
 }
--- src/test/java/br/com/rsinet/hub/tdd/testes/Testes.java
+++ src/test/java/br/com/rsinet/hub/tdd/testes/Testes.java
@@ -1,5 +1,6 @@
 package br.com.rsinet.hub.tdd.testes;
 
+import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 
 import br.com.rsi.hub.bdd.utils.Driver;
@@ -7,18 +8,18 @@ import br.com.rsinet.hub.tdd.pageObjects.CriarConta_PO;
 import br.com.rsinet.hub.tdd.pageObjects.Login_PO;
 
 public class Testes {
-	
-	public static void main(String[] args){
-		
-		//new Driver().AbriNavegadores();
-		
+
+	@Test
+	public void Teste() {
+
+		// new Driver().AbriNavegadores();
+
 		WebDriver driver;
-		
+
 		driver = new Driver().AbriNavegadores();
-		
+
 		new CriarConta_PO(driver).criarConta();
-		
-		
+
 //		try {
 //			new Login_PO(driver).entrarLogin("usuarioteste", "passw0rdTest");
 //		} catch (InterruptedException e) {
